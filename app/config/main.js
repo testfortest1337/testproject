1de2ebd4bd55597e8068d68e1f4054adc2805f7a
Vladyslav Frantsyan <frantsyan@MacBook-Air-Vladyslav.local>
2020-07-05 17:20:39 +0300

    rename const, edit render for MainTabs

--- app/config/main.js
+++ app/config/main.js
@@ -1,6 +1,6 @@
 export const PROJECT_NAME = 'Test Project';
 export const NEW_PEOPLE = 'New';
-export const All_PEOPLE = 'All';
+export const ALL_PEOPLE = 'All';
 export const FONT_COLOR = '#fff';
 export const BORDER_COLOR = '#fff';
 export const ACTIVE_COLOR = '#845EC2';
--- app/navigation/MainTabs.js
+++ app/navigation/MainTabs.js
@@ -3,7 +3,7 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import AllPeople from '../screens/AllPeople/AllPeople';
 import NewPeople from '../screens/NewPeople/NewPeople';
 import {
-  All_PEOPLE,
+  ALL_PEOPLE,
   NEW_PEOPLE,
   INACTIVE_COLOR,
   ACTIVE_COLOR,
@@ -11,38 +11,37 @@ import {
 import IMAGES from '../assets/imagesIndex';
 import TabIcon from '../components/TabIcon/TabIcon';
 
+const tabs = [
+  {name: ALL_PEOPLE, component: AllPeople, image: IMAGES.allPeopleIcon},
+  {name: NEW_PEOPLE, component: NewPeople, image: IMAGES.newPeopleIcon},
+];
+const tabBarOptions = {
+  activeTintColor: ACTIVE_COLOR,
+  inactiveTintColor: INACTIVE_COLOR,
+  labelStyle: {
+    fontSize: 14,
+    fontWeight: '600',
+  },
+};
 const Tab = createBottomTabNavigator();
+
+const renderItem = (item) => (
+  <Tab.Screen
+    key={item.name}
+    name={item.name}
+    component={item.component}
+    options={{
+      tabBarIcon: ({focused}) => (
+        <TabIcon source={item.image} isActive={focused} />
+      ),
+    }}
+  />
+);
 
 const MainTabs = () => {
   return (
-    <Tab.Navigator
-      initialRouteName={All_PEOPLE}
-      tabBarOptions={{
-        activeTintColor: ACTIVE_COLOR,
-        inactiveTintColor: INACTIVE_COLOR,
-        labelStyle: {
-          fontSize: 14,
-          fontWeight: '600',
-        },
-      }}>
-      <Tab.Screen
-        name={All_PEOPLE}
-        component={AllPeople}
-        options={{
-          tabBarIcon: ({focused}) => (
-            <TabIcon source={IMAGES.allPeopleIcon} isActive={focused} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name={NEW_PEOPLE}
-        component={NewPeople}
-        options={{
-          tabBarIcon: ({focused}) => (
-            <TabIcon source={IMAGES.newPeopleIcon} isActive={focused} />
-          ),
-        }}
-      />
+    <Tab.Navigator initialRouteName={ALL_PEOPLE} tabBarOptions={tabBarOptions}>
+      {tabs.map(renderItem)}
     </Tab.Navigator>
   );
 };
